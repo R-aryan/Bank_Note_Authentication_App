e2eb7bf941756181499a2023048d227376d0fa7c
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-09-25 20:41:26 +0530

    updated files for inference

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -1,4 +1,17 @@
 import pandas as pd
+import numpy as np
+import pickle
 
-df = pd.read_csv('../dataset/TestFile.csv')
-print(df.head())
+# df = pd.read_csv('../dataset/TestFile.csv')
+# print(df.head())
+
+
+class NotePredictor:
+
+    def __init__(self):
+        self.pickle_in = open("../training/note_classifier.pkl", "rb")
+        self.classifier = pickle.load(self.pickle_in)
+
+    def predict(self,variance,skewness,curtosis,entropy):
+
+
