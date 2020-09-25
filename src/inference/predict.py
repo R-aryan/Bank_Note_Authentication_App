e0a1da91de6389012c3f8b8610c5f3de09953925
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-09-25 20:45:13 +0530

    updated files for inference

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import pickle
 
+
 # df = pd.read_csv('../dataset/TestFile.csv')
 # print(df.head())
 
@@ -12,6 +13,10 @@ class NotePredictor:
         self.pickle_in = open("../training/note_classifier.pkl", "rb")
         self.classifier = pickle.load(self.pickle_in)
 
-    def predict(self,variance,skewness,curtosis,entropy):
+    def predict(self, variance, skewness, curtosis, entropy):
+        result = self.classifier.predict([[variance, skewness, curtosis, entropy]])
+        print(result)
 
 
+nap = NotePredictor()
+nap.predict(1.0552, 1.1857, -2.6411, 0.11033)
