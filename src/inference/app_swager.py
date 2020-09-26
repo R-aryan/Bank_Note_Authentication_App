b9f3232c934c54128f30dd0c7cf2b45b1218a64f
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-09-26 16:24:44 +0530

    updated files for inference

--- src/inference/app_swager.py
+++ src/inference/app_swager.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
+import numpy as np
+import pickle
 import pandas as pd
+import flasgger
+from flasgger import Swagger
 
 from src.inference.predict import NotePredictor
 
 app = Flask(__name__)
+Swagger(app)
 
 
 @app.route('/')
@@ -16,6 +21,32 @@ nap = NotePredictor()
 
 @app.route('/predict', methods=["GET"])
 def predict_note_authentication():
+    """Let's Authenticate the Banks Note
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: variance
+        in: query
+        type: number
+        required: true
+      - name: skewness
+        in: query
+        type: number
+        required: true
+      - name: curtosis
+        in: query
+        type: number
+        required: true
+      - name: entropy
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+
+    """
+
     variance = request.args.get("variance")
     skewness = request.args.get("skewness")
     curtosis = request.args.get("curtosis")
@@ -36,6 +67,20 @@ def predict_note_authentication():
 
 @app.route('/predict_file', methods=["POST"])
 def predict_note_file():
+    """Let's Authenticate the Banks Note
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+
+    responses:
+        200:
+            description: The output values
+
+    """
     df_test = pd.read_csv(request.files.get("file"))
     print(df_test.head())
     prediction = nap.predict_file(df_test)
--- src/inference/main.py
+++ src/inference/main.py
@@ -1,5 +1,6 @@
-from src.inference.flask_api import app
+from src.inference.app_swager import app
 
 
+# http://127.0.0.1:5000/apidocs/ url for running swagger
 if __name__ == '__main__':
     app.run(debug=True)
