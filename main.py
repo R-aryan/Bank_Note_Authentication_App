7a1ed4d97a562f4f66b0010f4b17fde716db96a9
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-09-27 00:14:55 +0530

    added files for inference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from  src.inference.app_swager import app
-
+from src.inference.app_swager import app
 
 # http://127.0.0.1:5000/apidocs/ url for running swagger
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(host='0.0.0.0', port=8000, debug=True)
